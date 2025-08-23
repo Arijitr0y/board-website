eaf03ef18afe064e2502fa403530687ed50caf4a
Arijit Roy <arijit1roy@gmail.com>
2025-08-23 20:37:46 +0000

    ok this for the customer part done for now now lets build a seperate adm

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,69 @@
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { DollarSign, Package, Users, FileCheck } from "lucide-react";
+  
+  export default function Dashboard() {
+    return (
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Total Revenue
+                    </CardTitle>
+                    <DollarSign className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">₹3,145,231.89</div>
+                    <p className="text-xs text-muted-foreground">
+                        +20.1% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Total Orders
+                    </CardTitle>
+                    <Package className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">+2350</div>
+                    <p className="text-xs text-muted-foreground">
+                        +180.1% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">New Customers</CardTitle>
+                    <Users className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">+122</div>
+                    <p className="text-xs text-muted-foreground">
+                        +19% from last month
+                    </p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Pending Reviews</CardTitle>
+                    <FileCheck className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">+57</div>
+                    <p className="text-xs text-muted-foreground">
+                        +1 since last hour
+                    </p>
+                </CardContent>
+            </Card>
+        </div>
+    )
+  }
+  
