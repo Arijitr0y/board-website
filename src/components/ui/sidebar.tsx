426305ee8738ff6ede47b1763b520137bd5c38d7
Arijit Roy <arijit1roy@gmail.com>
2025-08-23 20:56:10 +0000

    fix the side pannel ui it is cropping the main screen

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -25,7 +25,7 @@ const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 const SIDEBAR_WIDTH = "16rem"
 const SIDEBAR_WIDTH_MOBILE = "18rem"
-const SIDEBAR_WIDTH_ICON = "3rem"
+const SIDEBAR_WIDTH_ICON = "3.5rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 type SidebarContext = {
@@ -269,7 +269,7 @@ const SidebarInset = React.forwardRef<
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
-        "transition-[margin-left] duration-200 ease-in-out md:peer-data-[state=expanded]:ml-[--sidebar-width]",
+        "transition-[margin-left] duration-200 ease-in-out md:ml-[--sidebar-width-icon] md:peer-data-[state=expanded]:ml-[--sidebar-width]",
         className
       )}
       {...props}
