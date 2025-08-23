dce60aaa5a29cf7d4fe43a24edb8a1da3f1f2963
Arijit Roy <arijit1roy@gmail.com>
2025-08-23 21:26:34 +0000

    add a loading indicator when loading the page

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { Loader2 } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="flex h-[calc(100vh-8rem)] w-full items-center justify-center">
+      <Loader2 className="h-16 w-16 animate-spin text-primary" />
+    </div>
+  );
+}
