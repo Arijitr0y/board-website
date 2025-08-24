5114706ae80067ebd71066d06f56581b191babe3
Arijit Roy <arijit1roy@gmail.com>
2025-08-24 18:05:27 +0000

    in which page you added the scrolling banner

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -7,7 +7,7 @@ export default function OrderPage() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 bg-gray-50 dark:bg-gray-900/50">
         <Dashboard />
       </main>
     </div>
