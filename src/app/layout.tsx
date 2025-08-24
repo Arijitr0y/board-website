73fbaa2ec6501f57fe7ee98074d17865c7b20109
Arijit Roy <arijit1roy@gmail.com>
2025-08-24 17:57:04 +0000

    dont add the light and dark mode  thing in the customer website it will

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { CartProvider } from "@/context/cart-context";
 import { Footer } from '@/components/pcb-flow/footer';
-import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
   title: 'PCB Flow',
@@ -16,19 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="!scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-sans antialiased flex flex-col min-h-screen">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
           <CartProvider>
             <div className="flex-grow">
               {children}
@@ -36,7 +29,6 @@ export default function RootLayout({
             <Footer />
             <Toaster />
           </CartProvider>
-        </ThemeProvider>
       </body>
     </html>
   );
