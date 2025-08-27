7fd01b3f33e62522ab70c3195b4aab63b663f74e
Arijit Roy <arijit1roy@gmail.com>
2025-08-27 20:25:08 +0000

    dont add the dark light theme in the customer website

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -2,7 +2,6 @@
 import { CartProvider } from "@/context/cart-context";
 import { Footer } from '@/components/pcb-flow/footer';
 import { LoadingProvider, InstantLoadingIndicator } from '@/context/loading-context';
-import { ThemeProvider } from "@/components/theme-provider";
 
 export default function CustomerLayout({
   children,
@@ -10,23 +9,16 @@ export default function CustomerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <LoadingProvider>
-        <CartProvider>
-          <InstantLoadingIndicator />
-          <div className="flex flex-col min-h-screen">
-            <div className="flex-grow">
-              {children}
-            </div>
-            <Footer />
+    <LoadingProvider>
+      <CartProvider>
+        <InstantLoadingIndicator />
+        <div className="flex flex-col min-h-screen">
+          <div className="flex-grow">
+            {children}
           </div>
-        </CartProvider>
-      </LoadingProvider>
-    </ThemeProvider>
+          <Footer />
+        </div>
+      </CartProvider>
+    </LoadingProvider>
   );
 }
